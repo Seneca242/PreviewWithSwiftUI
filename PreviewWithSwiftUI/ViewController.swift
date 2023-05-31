9204be30fd53baf453db301b9803d94f64d8f58a
Seneca242 <flabbergasted242@gmail.com>
2023-05-31 10:56:22 +0300

    finished

--- PreviewWithSwiftUI/ViewController.swift
+++ PreviewWithSwiftUI/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 30, weight: .semibold)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+        setupConstraints()
     }
 
-
+    private func setupConstraints() {
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
 
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ViewController().showPreview()
+    }
+}
